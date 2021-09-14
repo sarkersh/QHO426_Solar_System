547a425e3fac88a2ad075e22e8bfc4573adfc206
sarkersh <86519060+sarkersh@users.noreply.github.com>
2021-09-15 00:12:26 +0100

    Update AE2 test file 2.py

--- AE2 test file 2.py
+++ AE2 test file 2.py
@@ -1,10 +1,10 @@
 import csv
 
-with open("data/sol_data1.csv") as f1:
+with open("../../../../Backup/QHO426/QHO426/Data/sol_data1.csv") as f1:
     f1.readline()
     for line in f1:
         print(line.split(",")[14])
 
-python_file = open("data\sol_data1.csv", "a")
+python_file = open("../../../../Backup/QHO426/QHO426/Data/sol_data1.csv", "a")
 python_file.write(' ')
 python_file.close()
