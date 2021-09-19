6d9c0eeb909c7cb5744f4c4230fc14b70f852c7a
sarkersh <86519060+sarkersh@users.noreply.github.com>
2021-09-19 16:42:58 +0100

    Initial Commit Initial commit

--- tui.py
+++ tui.py
@@ -28,8 +28,6 @@ def menu():
     :return: None if invalid selection otherwise an integer corresponding to a valid selection
     """
     # TODO: Your code here
-    # ans = True
-    # while ans:
     x = 'Main Menu'
     print("-" * len(x), x, "-" * len(x))
     print("""
@@ -48,47 +46,6 @@ def menu():
             return None
     except:
         return None
-
-        # ans = input("Please input string as display above on main menu, e.g.\"Load Data\":- ")
-        # if ans == "Load Data":
-        #     print("\n 2"), print("Data loading has started"), print("Data loading has completed")
-        # elif ans == "Process Data":
-        #     print("\n 2"), print("Process Data has started"), print("Process Data has completed")
-        # elif ans == "Visualise Data":
-        #     print("\n 3"), print("Visualise Data has started"), print("Visualise Data has completed")
-        # elif ans == "Save Data":
-        #     print("\n 4"), print("Saving Data started"), print("Saving Data completed")
-        # elif ans == "Exit":
-        #     print("\n 5")
-        #     ans = None
-        # else:
-        #     print("\n Not Valid Choice Try again")
-    # Tutor Replit:
-    # print("Main Menu")
-    #
-    # usr = input("\nLoad Data\nProcess Data\nVisualise Data\nSave Data\nExit\n\nPlease enter your choice as displayed: ")
-    #
-    # if (usr == "Load Data"):
-    #     i = 1
-    #
-    # elif (usr == "Process Data"):
-    #     i = 2
-    #
-    # elif (usr == "Visualise Data"):
-    #     i = 3
-    #
-    # elif (usr == "Save Data"):
-    #     i = 4
-    #
-    # elif (usr == "Exit"):
-    #     return 0
-    #
-    # else:
-    #     print("Invalid Choice!")
-    #     return None
-    #
-    # return i
-
 
 #menu()
 
@@ -153,9 +110,6 @@ def source_data_path():
     :return: None if the file path does not end in 'csv' otherwise return the file path entered by the user
     """
     # TODO: Your code here
-    # import pathlib, os
-    #import os
-    # user_input = input("Please enter the file path: ")
     filepath = input("Please enter the file path: ")
     if (filepath.endswith("csv")):
         return filepath
@@ -163,38 +117,7 @@ def source_data_path():
         error("The file entered is not csv file!")
         return None
 
-    # assert os.path.exists(user_input), "File not fount at, "+str(user_input)
-    # f = open(user_input,'r+')
-    # print("File found")
-    # f.close()
-    # file = pathlib.Path(user_input)
-    # if file.exists():
-    # if os.path.exists(user_input):
-    #     # print(os.path.realpath(file.name)), print("File Found")
-    #     print(os.path.realpath(user_input)), print("File Found")  # (GeeksforGeeks, 2019)
-    # else:
-    #     if user_input.endswith("csv"):
-    #         error("File not found")
-    #         print("Path location", "\"", os.path.realpath('data\sol_data.csv'), "\"")
-    #         print("Path contains: ", os.listdir('data'))  # (learn.solent.ac.uk, n.d.)
-    #         source_data_path()
-    #     else:
-    #         error("File not found"), print("File must be ends with \"csv\"") \
-    #             , print("Path location", "\"", os.path.realpath('data\sol_data.csv'), "\"")
-    #         print("Path contains: ", os.listdir('data'))
-    #         source_data_path()
-    # Tutor replit:
-    # path = input("Please enter a file path for the data file:\n")
-    #
-    # if path.endswith(".csv"):
-    #     return path
-    # return None
-
-
-# www.guru99.com. (n.d.). Python Check If File or Directory Exists. [online] Available at:https://www.guru99.com/python-check-if-file-exists.html.
-# learn.solent.ac.uk. (n.d.). Solent Online Learning: Log in to the site. [online] Available at: https://learn.solent.ac.uk/mod/page/view.php?id=2049756 [Accessed 4 Sep. 2021].
-
-#source_data_path()
+# source_data_path()
 
 def process_type():
     """
@@ -213,8 +136,6 @@ def process_type():
     :return: None if an invalid selection made otherwise an integer corresponding to a valid option
     """
     # TODO: Your code here
-    # ans = True
-    # while ans:
     x = "Processing Menu"
     print("\n\t\t", "-" * len(x), x, "-" * len(x))
     print("""
@@ -234,44 +155,8 @@ def process_type():
             return None
     except:
         return None
-        # ans = input("What would you like to do? :- ")
-        # if ans == "Retrieve entity":
-        #     print("\n 1")
-        # elif ans == "Retrieve entity details":
-        #     print("\n 2")
-        # elif ans == "Categorise entities by type":
-        #     print("\n 3")
-        # elif ans == "Categorise entities by gravity":
-        #     print("\n 4")
-        # elif ans == "Summarise entities by orbit":
-        #     print("\n 5")
-        #     ans = None
-        # else:
-        #     print("\n Not Valid Choice Try again")
-    # Tutor replit:
-    # print("How would you like for the file to be processed?")
-    #
-    # usr = input(
-    #     "\nRetrieve entity\nRetrieve entity details\nCategorise entities by type\nCategorise entities by gravity\nSummarise entitites by orbit\n\nPlease enter your choice as listed: ")
-    #
-    # if (usr == "Retrieve entity"):
-    #     i = 1
-    # elif (usr == "Retrieve entity details"):
-    #     i = 2
-    # elif (usr == "Categorise entities by type"):
-    #     i = 3
-    # elif (usr == "Categorise entitites by gravity"):
-    #     i = 4
-    # elif (usr == "Summarise entities by orbit"):
-    #     i = 5
-    # else:
-    #     print("Invalid input")
-    #     return None
-    #
-    # return i
 
-
-#process_type()
+# process_type()
 
 def entity_name():
     """
@@ -285,10 +170,7 @@ def entity_name():
     # TODO: Your code here
     name = input("Please type the name of an entity: ")
     return name
-    #print(name)
-    # Tutor Replit:
-    # entity = input("Please enter the name of an entity:\n")
-    # return entity
+
 
 
 #entity_name()
@@ -317,28 +199,7 @@ def entity_details():
             continue
     return [entityName, columnList]
 
-    # entityName = input('Enter name of entity: ')
-    # columnList = list(map(int, input("Integer columns: ").split(',')))  # (GeeksforGeeks, 2019)
-    # return entityName, columnList
-    # return [a, b]
-    # x = entityName, columnList
-    # print(list(x))
-    # Tutor replit:
-    # entity_n = input("Please enter the name of an entity:\n")
-    # entity_list = input("Please enter a list of integer column indexes: ")
-    #
-    # list = entity_list.split(",")
-    # print("list: ", list)
-    #
-    # compl_l = []
-    # for i in list:
-    #     compl_l.append(int(i))
-    #
-    # return entity_n, compl_l
-
-
 #print(entity_details())
-# GeeksforGeeks. (2019). Python | Get a list as input from user. [online] Available at: https://www.geeksforgeeks.org/python-get-a-list-as-input-from-user/?ref=leftbar-rightbar [Accessed 4 Sep. 2021].
 
 def list_entity(entity, cols=[]):
     """
@@ -370,40 +231,6 @@ def list_entity(entity, cols=[]):
     else:
         print(entity)
     return
-    # Week 8.4 - More Gremlins.txt
-    # def list_entity(entity, cols=[]):
-    #     if len(cols) > 0:
-    #         #list_b = [entity[a] for a in cols]
-    #         list_b = []
-    #         for z in cols:
-    #             list_b.append(entity[z])
-    #         print(list_b)
-    #     else:
-    #         print(entity)
-    #     return
-    # list_entity(['Earth', True, 9.8, 5], [0, 2, 3])
-
-#     if len(cols) > 0:
-#         list = [entity[z] for z in cols]
-#         print(list)
-#     else:
-#         print(entity)
-#     return
-#     Tutor replit:
-#     if cols is None:
-#         cols = []
-#     if len(cols) == 0:
-#         print(entity)
-#     else:
-#         print("[", end="")
-#         for i in range(len(cols)):
-#             if i == len(cols) - 1:
-#                 print(f"{entity[cols[i]]}", end="")
-#             print(f"{entity[cols[i]]}", end=",")
-#         print("]")
-#
-#     return None
-
 
 # list_entity(['Earth', True, 9.8, 0, 8], [0,5])
 
@@ -437,18 +264,6 @@ def list_entities(entities, cols=[]):
     else:
         print(entities)
     return
-    # Tutor replit:
-    # for list in entities:
-    #     if len(cols) == 0:
-    #         for j in list:
-    #             print(j, end=", ")
-    #         print("\n")
-    #
-    #     else:
-    #         for i in cols:
-    #             print(list[i], end=", ")
-    #     return None
-
 
 #list_entities(['Earth', True, 9.8, 0, 8], [0, 1])
 
@@ -496,31 +311,6 @@ def gravity_range():
     listGravityRange = [minGravity, maxGravity]
     tupleGravityRange = tuple(listGravityRange)
     return tupleGravityRange
-    # Cousin code:
-    # lowerInp = input("Enter lower limit for gravity :")
-    # upperInp = input("Enter upper limit for gravity :")
-    # lst = []
-    # while float(lowerInp) < float(upperInp):
-    #     lst.append(float("{:.2f}".format(float(lowerInp))))
-    #     lowerInp = float(lowerInp) + 0.1
-    # return lst
-
-    # t = tuple()
-    # n = int(input("Total number of values in tuple:- "))
-    # for i in range(n):
-    #     a = float(input("Enter elements:- "))
-    #     t = t + (a,)
-    # return min(t), max(t)
-    # Tutor replit:
-    # list = []
-    # upper_limit = input("Please enter the upper limit for gravity: ")
-    # list.append(upper_limit)
-    #
-    # lower_limit = input("Please enter the lower limit for gravity: ")
-    # list.append(lower_limit)
-    #
-    # tupl = tuple(list)
-    # return tupl
 
 
 # print(gravity_range())
@@ -541,43 +331,7 @@ def orbits():
     entityNames = input('Enter entity names(e.g. Jupiter,Earth,Mars): ')
     entityNameList = entityNames.split(",")
     return entityNameList
-    # import csv
-    # with open('data\sol_data.csv', 'r') as f1, open('data\sol_data.csv', 'a') as f2:
-    #     reader = csv.reader(f1)
-    #     next(reader, None)
-    #     orbits = [21]
-    #
-    #     for row in reader:
-    #         content = list(row[i] for i in orbits)
-    #         # print(content)
-    #
-    #         # for z in content:
-    #         #     print(z)
-    #         # print(content)
-    #     # x = list(reader)
-    #     # print(x)
-    #     writer = csv.writer(f2)
-    #     next(reader, None)
-    #     orbits = [21]
-    #     orbits = [str(cols) for cols in input("Enter a list of entity names : ").split(",")]
-    #     print(list(orbits, content))
-    # x = input("Enter a list of entity names : ").split(",")
-    # return x
-    #
-    # import pandas as pd
-    # df = pd.read_csv('data/sol_data.csv')
-    # df = df.iloc[:, [21]]
-    # print(df)
-    # x = []
-    # x = [str(cols) for cols in input("Enter a list of entity names : ").split(",")]
-    # print(x)
-    # Tutor replit:
-    # n = int(input("Please enter the amount of entities that you wish to input: "))
-    # list = []
-    # for i in range(0, n):
-    #     data = input(f"Please enter entity number {i}: ")
-    #     list.append(data)
-    # return list
+
 
 
 #print(orbits())
@@ -620,32 +374,6 @@ def visualise():
     except:
         return None
 
-        # ans = input("\t\tWhat would you like to do?:- ")
-        # if ans == "Entities by type":
-        #     print("\n\t\t1")
-        # elif ans == "Entities by gravity":
-        #     print("\n\t\t2")
-        # elif ans == "Summary of orbits":
-        #     print("\n\t\t3")
-        # elif ans == "Animate gravities":
-        #     print("\n\t\t4")
-        #     ans = None
-        # else:
-        #     print("\n Not Valid Choice Try again")
-    # Tutor replit:
-    # usr_choice = int(input("Please chose how the data should be visualised:\n\n"
-    #                        "1 - Entities by type\n"
-    #                        "2 - Entities by gravity\n"
-    #                        "3 - Summary of orbits\n"
-    #                        "4 - Animate gravities\n"
-    #                        ""
-    #                        "\nPlease enter a number corresponding to your choice: "))
-    # if usr_choice == 1 or usr_choice == 2 or usr_choice == 3 or usr_choice == 4:
-    #     return usr_choice
-    # else:
-    #     print("Invalid choice!")
-    #     return None
-
 
 #visualise()
 
@@ -667,8 +395,6 @@ def save():
     # TODO: Your code here
     x = 'Saving Menu'
     print("\n\t\t", "-" * len(x), x, "-" * len(x))
-    # ans = True
-    # while ans:
     print("""
         How would you like for the data to be save\n\t\t(e.g.\"1\")?
         1.Export as JSON
